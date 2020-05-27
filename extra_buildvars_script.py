fd13607384dcde093da6789671ee0a7f3c362110
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-05-27 17:18:32 +0200

    Change build with 2 environments

---
+++ extra_buildvars_script.py
@@ -0,0 +1,10 @@
+import datetime
+
+Import("env")
+
+timestamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+print defines
+
+env.Replace(PROGNAME="s0device-monitor_%s_v%s" % (defines.get("ENVIRONMENT"), timestamp))
