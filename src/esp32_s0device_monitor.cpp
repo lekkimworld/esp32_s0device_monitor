810c966eb63a218ffc5ad059f05f0a330a1d3b02
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-09-21 14:14:40 +0200

    Working with final PCB

--- src/esp32_s0device_monitor.cpp
+++ src/esp32_s0device_monitor.cpp
@@ -11,7 +11,8 @@
 #include "credentials.h"
 
 // DST Root CA X3, https://letsencrypt.org/certs/trustid-x3-root.pem.txt
-const char* rootCACertificate_DSTRootCAX3 = \
+// used on production dyno on Heroku (own domain)
+const char* rootCACertificate_DSTRootCAX3_HerokuCustomDoamin = \
 "-----BEGIN CERTIFICATE-----\n" \
 "MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n" \
 "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
@@ -33,42 +34,9 @@ const char* rootCACertificate_DSTRootCAX3 = \
 "Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" \
 "-----END CERTIFICATE-----\n";
 
-// Let's Encrypt, SRG Root X1 (self-signed), https://letsencrypt.org/certs/isrgrootx1.pem.txt
-const char* rootCACertificate_LetsEncrypt = \
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-"-----END CERTIFICATE-----\n";
-
 // DigiCert High Assurance EV Root CA, https://www.digicert.com/digicert-root-certificates.htm
-const char* rootCACertificate_Digicert = \
+// used for non-production dynos on Heroku
+const char* rootCACertificate_Digicert_HerokuDefaultDomain = \
 "-----BEGIN CERTIFICATE-----\n" \
 "MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs\n" \
 "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
@@ -92,7 +60,6 @@ const char* rootCACertificate_Digicert = \
 "vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep\n" \
 "+OkuE6N36B9K\n" \
 "-----END CERTIFICATE-----\n";
-const char* rootCACertificate = rootCACertificate_Digicert;
 
 // declaration
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
@@ -102,7 +69,8 @@ int page = 0;
 unsigned long SAMPLE_DURATION = 2 * 60 * 1000;
 unsigned long samplePeriodStart = 0;
 bool justReset = true;
-bool useDisplay = false;
+bool useDisplay = true;
+const char* rootCACertificate = rootCACertificate_Digicert_HerokuDefaultDomain;
 
 // Create a new syslog instance with LOG_KERN facility
 WiFiUDP udpClient;
@@ -405,27 +373,27 @@ void setup() {
   printMacAddress();
 
   // setup plugs and connections per plug
-  strcpy(plugs[0].name, "Yellow RJ45");
-  strcpy(plugs[0].devices[DEVICE_IDX_ORANGE].name, "Tumbler (Or)   : %d");
-  strcpy(plugs[0].devices[DEVICE_IDX_ORANGE].id,   "P0D0"); //"s0dryer");
-  strcpy(plugs[0].devices[DEVICE_IDX_BROWN].name,  "Vaskemask. (Br): %d");
-  strcpy(plugs[0].devices[DEVICE_IDX_BROWN].id,    "P0D1"); //"s0washer");
-  strcpy(plugs[0].devices[DEVICE_IDX_GREEN].name,  "Opvask. (Gr)   : %d");
-  strcpy(plugs[0].devices[DEVICE_IDX_GREEN].id,    "P0D2"); //"s0dishwasher");
-  strcpy(plugs[0].devices[DEVICE_IDX_BLUE].name,   "Varmepumpe (Bl): %d");
-  strcpy(plugs[0].devices[DEVICE_IDX_BLUE].id,     "P0D3"); //"s0heatpump");
+  strcpy(plugs[0].name, "Plug 0");
+  strcpy(plugs[0].devices[DEVICE_IDX_ORANGE].name,  "P0D0 (Or): %d");
+  strcpy(plugs[0].devices[DEVICE_IDX_ORANGE].id,    "P0D0"); //"s0dryer");
+  strcpy(plugs[0].devices[DEVICE_IDX_BROWN].name,   "P0D1 (Br): %d");
+  strcpy(plugs[0].devices[DEVICE_IDX_BROWN].id,     "P0D1"); //"s0washer");
+  strcpy(plugs[0].devices[DEVICE_IDX_BLUE].name,    "P0D2 (Bl): %d");
+  strcpy(plugs[0].devices[DEVICE_IDX_BLUE].id,      "P0D2"); //"s0heatpump");
+  strcpy(plugs[0].devices[DEVICE_IDX_GREEN].name,   "P0D3 (Gr): %d");
+  strcpy(plugs[0].devices[DEVICE_IDX_GREEN].id,     "P0D3"); //"s0dishwasher");
   plugs[0].activeDevices = 4;
 
-  strcpy(plugs[1].name, "White RJ45");
-  strcpy(plugs[1].devices[DEVICE_IDX_ORANGE].name, "Gulvvame, bad  : %d");
-  strcpy(plugs[1].devices[DEVICE_IDX_ORANGE].id,   "P1D0"); //"s0floorheat");
-  strcpy(plugs[1].devices[DEVICE_IDX_BROWN].name, "Gulvvame, bad  : %d");
-  strcpy(plugs[1].devices[DEVICE_IDX_BROWN].id,   "P1D1"); //"s0floorheat");
-  strcpy(plugs[1].devices[DEVICE_IDX_GREEN].name, "Gulvvame, bad  : %d");
-  strcpy(plugs[1].devices[DEVICE_IDX_GREEN].id,   "P1D2"); //"s0floorheat");
-  strcpy(plugs[1].devices[DEVICE_IDX_BLUE].name, "Gulvvame, bad  : %d");
-  strcpy(plugs[1].devices[DEVICE_IDX_BLUE].id,   "P1D3"); //"s0floorheat");
-  plugs[1].activeDevices = 4;
+  strcpy(plugs[1].name, "Plug 1");
+  strcpy(plugs[1].devices[DEVICE_IDX_ORANGE].name,  "Gulvvame, bad  : %d");
+  strcpy(plugs[1].devices[DEVICE_IDX_ORANGE].id,    "P1D0"); //"s0floorheat");
+  strcpy(plugs[1].devices[DEVICE_IDX_BROWN].name,   "Gulvvame, bad  : %d");
+  strcpy(plugs[1].devices[DEVICE_IDX_BROWN].id,     "P1D1"); //"s0floorheat");
+  strcpy(plugs[1].devices[DEVICE_IDX_BLUE].name,    "Gulvvame, bad  : %d");
+  strcpy(plugs[1].devices[DEVICE_IDX_BLUE].id,      "P1D2"); //"s0floorheat");
+  strcpy(plugs[1].devices[DEVICE_IDX_GREEN].name,   "Gulvvame, bad  : %d");
+  strcpy(plugs[1].devices[DEVICE_IDX_GREEN].id,     "P1D3"); //"s0floorheat");
+  plugs[1].activeDevices = 1;
 
   S0_LOG_INFO("Initializing pins");
   initISR();
